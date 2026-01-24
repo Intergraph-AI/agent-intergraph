b9e3ac87bbfa1128ee4a4f88e5b7c72de94257ce
Amit Rathore <amit@karate.local>
2026-01-24 15:38:12 -0800

    Initial commit: Scaffold agent-intergraph project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to agent-intergraph
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/agent_intergraph/core_test.clj
@@ -0,0 +1,7 @@
+(ns agent-intergraph.core-test
+  (:require [clojure.test :refer :all]
+            [agent-intergraph.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
